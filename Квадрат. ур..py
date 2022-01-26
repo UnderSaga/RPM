1d8c5004f914a4fbef882c145c84d9e18b8b036b
UnderSaga <98108699+UnderSaga@users.noreply.github.com>
2022-01-26 08:40:25 +0300

    Update Квадрат. ур..py

--- Квадрат. ур..py
+++ Квадрат. ур..py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     if a != 0 and b != 0 and c != 0:
         D = b ^ 2 - 4 * a * c
         if D > 0:
-            x1 = -b + sqrt(D) / 2 * a
-            x2 = -b - sqrt(D) / 2 * a
+            x1 = (-b + sqrt(D)) / (2 * a)
+            x2 = (-b - sqrt(D)) / (2 * a)
             print (x1, x2)
         elif D == 0:
             x = -b / 2 * a
